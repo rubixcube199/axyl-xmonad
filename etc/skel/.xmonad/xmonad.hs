450651934877e787f77d809269b3ee31173633d4
Kungger-git <matthewriel63@gmail.com>
2021-10-25 21:49:35 +0800

    minor change

--- etc/skel/.xmonad/xmonad.hs
+++ etc/skel/.xmonad/xmonad.hs
@@ -54,79 +54,76 @@ myKeys conf@(XConfig {XMonad.modMask = super}) = M.fromList $
     , ((super, xK_Escape), spawn "xkill")
     
     -- Change gaps on the fly
-    , ((super .|. controlMask, 	xK_g), sendMessage $ ToggleGaps)               					-- toggle all gaps
-    , ((super .|. shiftMask, 	xK_g), sendMessage $ setGaps [(L,50), (R,50), (U,80), (D,50)]) 	-- reset the GapSpec
+    , ((super .|. controlMask, xK_t), sendMessage $ IncGap 10 L)     -- increment the left-hand gap
+    , ((super .|. shiftMask, xK_t), sendMessage $ DecGap 10 L)     -- decrement the left-hand gap
     
-    , ((super .|. controlMask, 	xK_t), sendMessage $ IncGap 10 L)     -- increment the left-hand gap
-    , ((super .|. shiftMask, 	xK_t), sendMessage $ DecGap 10 L)     -- decrement the left-hand gap
+    , ((super .|. controlMask, xK_y), sendMessage $ IncGap 10 U)     -- increment the top gap
+    , ((super .|. shiftMask, xK_y), sendMessage $ DecGap 10 U)     -- decrement the top gap
     
-    , ((super .|. controlMask, 	xK_y), sendMessage $ IncGap 10 U)     -- increment the top gap
-    , ((super .|. shiftMask, 	xK_y), sendMessage $ DecGap 10 U)     -- decrement the top gap
-    
-    , ((super .|. controlMask, 	xK_u), sendMessage $ IncGap 10 D)     -- increment the bottom gap
-    , ((super .|. shiftMask, 	xK_u), sendMessage $ DecGap 10 D)     -- decrement the bottom gap
+    , ((super .|. controlMask, xK_u), sendMessage $ IncGap 10 D)     -- increment the bottom gap
+    , ((super .|. shiftMask, xK_u), sendMessage $ DecGap 10 D)     -- decrement the bottom gap
 
-    , ((super .|. controlMask, 	xK_i), sendMessage $ IncGap 10 R)     -- increment the right-hand gap
-    , ((super .|. shiftMask, 	xK_i), sendMessage $ DecGap 10 R)     -- decrement the right-hand gap
+    , ((super .|. controlMask, xK_i), sendMessage $ IncGap 10 R)     -- increment the right-hand gap
+    , ((super .|. shiftMask, xK_i), sendMessage $ DecGap 10 R)     -- decrement the right-hand gap
 
 	-- Window Manager Specific -----------------------------------------
 
     -- Resize viewed windows to the correct size
-    , ((super,                   xK_r), 		refresh)
+    , ((super, xK_r), refresh)
 
     -- Move focus to the master window
-    , ((super,               	xK_m), 		windows W.focusMaster)
+    , ((super, xK_m), windows W.focusMaster)
 
     -- Swap the focused window and the master window
-    , ((super,               	xK_s), 		windows W.swapMaster)
+    , ((super, xK_s), windows W.swapMaster)
 
     -- Push window back into tiling
-    , ((super,               	xK_t),		withFocused $ windows . W.sink)
+    , ((super, xK_t), withFocused $ windows . W.sink)
 
     -- Rotate through the available layout algorithms
-    , ((super,               xK_space), 		sendMessage NextLayout)
+    , ((super, xK_space), sendMessage NextLayout)
 
     --  Reset the layouts on the current workspace to default
-    , ((super .|. shiftMask, xK_space), 		setLayout $ XMonad.layoutHook conf)
+    , ((super .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf)
 
     -- Move focus to the next window
-    , ((super,                 xK_Tab), 		windows W.focusDown)
+    , ((super, xK_Tab), windows W.focusDown)
 
     -- Move focus to the next window
-    , ((super,               	xK_j), 		windows W.focusDown)
-    , ((super,                xK_Left), 		windows W.focusDown)
+    , ((super, xK_j), windows W.focusDown)
+    , ((super, xK_Left), windows W.focusDown)
 
     -- Move focus to the previous window
-    , ((super,               	xK_k), 		windows W.focusUp)
-    , ((super,               xK_Right), 		windows W.focusUp)
+    , ((super, xK_k), windows W.focusUp)
+    , ((super, xK_Right), windows W.focusUp)
 
     -- Swap the focused window with the next window
-    , ((super .|. shiftMask, 	xK_j),		windows W.swapDown)
-    , ((super .|. shiftMask, 	xK_Left),	windows W.swapDown)
+    , ((super .|. shiftMask, xK_j), windows W.swapDown)
+    , ((super .|. shiftMask, xK_Left), windows W.swapDown)
 
     -- Swap the focused window with the previous window
-    , ((super .|. shiftMask, 	xK_k),		windows W.swapUp)
-    , ((super .|. shiftMask, 	xK_Right),	windows W.swapUp)
+    , ((super .|. shiftMask, xK_k), windows W.swapUp)
+    , ((super .|. shiftMask, xK_Right), windows W.swapUp)
 
     -- Shrink the master area
-    , ((super,               	xK_h),		sendMessage Shrink)
-    , ((super .|. controlMask,   xK_Left),	sendMessage Shrink)
+    , ((super, xK_h), sendMessage Shrink)
+    , ((super .|. controlMask, xK_Left), sendMessage Shrink)
 
     -- Expand the master area
-    , ((super,               	xK_l),		sendMessage Expand)
-    , ((super .|. controlMask,   xK_Right),	sendMessage Expand)
+    , ((super, xK_l), sendMessage Expand)
+    , ((super .|. controlMask, xK_Right), sendMessage Expand)
 
     -- Increment the number of windows in the master area
-    , ((super, 					xK_comma),		sendMessage (IncMasterN 1))
+    , ((super, xK_comma), sendMessage (IncMasterN 1))
 
     -- Deincrement the number of windows in the master area
-    , ((super,					xK_period),		sendMessage (IncMasterN (-1)))
+    , ((super, xK_period), sendMessage (IncMasterN (-1)))
 
     -- Restart xmonad
-    , ((controlMask .|. shiftMask, 						 xK_r),		spawn "xmonad --recompile; xmonad --restart")
+    , ((controlMask .|. shiftMask, xK_r), spawn "xmonad --recompile; xmonad --restart")
     
     -- Quit xmonad
-    , ((controlMask .|. shiftMask, 						 xK_q),		spawn "pkill -KILL -u $USER")
+    , ((controlMask .|. shiftMask, xK_q), spawn "pkill -KILL -u $USER")
 
     ]
     ++
